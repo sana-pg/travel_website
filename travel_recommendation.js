3a6eef17936dc7fa31372d667d32ccf5ce2b7a77
sana-pg <khalils@pg.com>
2024-08-05 08:42:02 -0400

    added json navigation

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -5,11 +5,10 @@ const patients = [];
 
 
 function resetForm() {
-    document.getElementById("name").value = "";
-    document.querySelector('input[name="gender"]:checked').checked = false;
-    document.getElementById("age").value = "";
-    document.getElementById("condition").value = "";
+    document.getElementById("recommendations_search").value = "";
   }
+
+reset.addEventListener('click', resetForm);
 
 function searchCondition() {
     const input = document.getElementById('recommendations_search').value.toLowerCase();
@@ -19,23 +18,30 @@ function searchCondition() {
     fetch('travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
+            const countries = data.countries;
+            const temples = data.temples;
+            const beaches = data.beaches;
 
-        console.log(data.countries)
-        const country = data.countries.find(item => item.name === input);
-
-        if (country) {
-            const name = country.name.join(', ');
-            const cities = country.cities.join(', ');
-
-            resultDiv.innerHTML += `<h2>${country.name}</h2>`;
-            resultDiv.innerHTML += `<img src="${country.cities}" alt="hjh">`;
-
-            resultDiv.innerHTML += `<p><strong>Name:</strong> ${name}</p>`;
-            resultDiv.innerHTML += `<p><strong>Cities:</strong> ${cities}</p>`;
-        } else {
-            resultDiv.innerHTML = 'Country not found.';
-        }
-        })
+            if (input === "countries") {
+                // Display the list of countries
+                countries.forEach(country => {
+                resultDiv.innerHTML += `<h2>${country.name}</h2>`;
+                });
+            } else if (input === "temples") {
+                // Display the list of temples
+                resultDiv
+                temples.forEach(temple => {
+                resultDiv.innerHTML += `<h2>${temple.name}</h2>`;
+                });
+            } else if (input === "beaches") {
+                // Display the list of beaches
+                beaches.forEach(beach => {
+                resultDiv.innerHTML += `<h2>${beach.name}</h2>`;
+                });
+            } else {
+                resultDiv.innerHTML = 'Country not found.';
+            }})
+        
         .catch(error => {
         console.error('Error:', error);
         resultDiv.innerHTML = 'An error occurred while fetching data.';
